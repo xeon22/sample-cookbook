098389752c61ea91e8d36313775fc64ff5cbdbcf
txag1995 <bryanj@pcsoln.com>
2015-03-23 10:37:28 -0500

    Update clowns_spec.rb

--- test/integration/default/serverspec/localhost/clowns_spec.rb
+++ test/integration/default/serverspec/localhost/clowns_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'clowns site' do
-  it 'responds on port 8000' do
-    expect(port 8000).to be_listening 'tcp'
+  it 'responds on port 80' do
+    expect(port 80).to be_listening 'tcp'
   end
 
   it 'returns clowns in the HTML body' do
-    expect((command 'curl localhost:8000').stdout).to match(/clowns/)
+    expect((command 'curl localhost:80').stdout).to match(/clowns/)
   end
 end
